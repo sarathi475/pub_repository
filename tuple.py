1b4ce6b5d496ebe4ae6185ea50ee2063fd44df7a
parthadata <sarathi475@gmail.com>
2018-03-11 21:59:39 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+sara=['partha','sarathi','kathirvel','banu',23,9,475]
+saratuple=['sara',123]
+print sara
+print sara[0]
+print sara[2:]
+print sara[2:5]
+print sara *2
+print sara +saratuple
+
+#defrence between two are same but tuple read only so not assign value replace
+#print sara[2]=usha  -it was not posible to change tuple
+#print sara[2]=usha -it was posible to change list
+
+
+
